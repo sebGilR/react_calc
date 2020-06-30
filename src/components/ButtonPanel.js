f71bafbdbf52bfcbe29835ccd853eb4a5f9bd7c4
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-06-30 16:36:57 -0500

    Change button text size and cursor

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,32 +5,32 @@ function ButtonPanel() {
   return (
     <div className='buttonPanel'>
       <div className='button_group'>
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name="%" />
+        <Button name="AC" color='lightgray' />
+        <Button name="+/-" color='lightgray' />
+        <Button name="%" color='lightgray' />
         <Button name="÷" />
       </div>
       <div className='button_group'>
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
+        <Button name="7" color='lightgray' />
+        <Button name="8" color='lightgray' />
+        <Button name="9" color='lightgray' />
         <Button name="X" />
       </div>
       <div className='button_group'>
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
+        <Button name="4" color='lightgray' />
+        <Button name="5" color='lightgray' />
+        <Button name="6" color='lightgray' />
         <Button name="-" />
       </div>
       <div className='button_group'>
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="2" />
+        <Button name="1" color='lightgray' />
+        <Button name="2" color='lightgray' />
+        <Button name="2" color='lightgray' />
         <Button name="+" />
       </div>
       <div className='button_group'>
-        <Button name="0" wide={true} />
-        <Button name="." />
+        <Button name="0" wide={true} color='lightgray' />
+        <Button name="." color='lightgray' />
         <Button name="=" />
       </div>
     </div>
