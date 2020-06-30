958962e30b069d65cf35a456d8d2d306bb3b4b07
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-06-30 18:58:34 -0500

    Convert app to stateful class component pass parameters required by children elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 // import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div id="calculator">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick = (buttonName) => this.setState(calculate(this.state), buttonName);
+
+  render() {
+    return (
+      <div id="calculator">
+        <Display result={this.state.total || this.state.next} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    )
+  };
+};
 
 export default App;
