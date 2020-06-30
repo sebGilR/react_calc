99e22cf00ae9623133e5d9c10a2138f2894eac1e
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-06-30 15:56:33 -0500

    Style button groups

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,31 +4,31 @@ import Button from './Button';
 function ButtonPanel() {
   return (
     <div className='buttonPanel'>
-      <div>
+      <div className='button_group'>
         <Button name="AC" />
         <Button name="+/-" />
         <Button name="%" />
         <Button name="÷" />
       </div>
-      <div>
+      <div className='button_group'>
         <Button name="7" />
         <Button name="8" />
         <Button name="9" />
         <Button name="X" />
       </div>
-      <div>
+      <div className='button_group'>
         <Button name="4" />
         <Button name="5" />
         <Button name="6" />
         <Button name="-" />
       </div>
-      <div>
+      <div className='button_group'>
         <Button name="1" />
         <Button name="2" />
         <Button name="2" />
         <Button name="+" />
       </div>
-      <div>
+      <div className='button_group'>
         <Button name="0" />
         <Button name="." />
         <Button name="=" />
